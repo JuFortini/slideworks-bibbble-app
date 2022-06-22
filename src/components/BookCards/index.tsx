9cfc77b2b70a5ed857cfcc697a2edfe3131deeff
Jucimara Fortini <jucimaraafortini@gmail.com>
2022-06-21 22:23:41 -0300

    book cards layout

--- src/components/BookCards/index.tsx
+++ src/components/BookCards/index.tsx
@@ -1,6 +1,15 @@
-import { Box, Img, Text } from "@chakra-ui/react";
+import { Box, Flex, Img, Stack, Text } from "@chakra-ui/react";
 
-export function BookCards() {
+interface BookCardsProps {
+  image: string,
+  title: string,
+  author: string,
+  description: string,
+  genre: string,
+  publishedAt: string,
+}
+
+export function BookCards({ image, title, author, description, genre, publishedAt }: BookCardsProps) {
   return (
     <Box 
       w="16rem" 
@@ -11,8 +20,32 @@ export function BookCards() {
       borderColor="gray.100"
     >
       <Box m="1rem">
-        <Img src="https://images.unsplash.com/photo-1532012197267-da84d127e765?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80" />
-        <Text>Nome do livro</Text>
+        <Img 
+          src={image} 
+          w="14.2rem"
+          h="14rem"
+          objectFit="cover"
+          borderRadius="4px"
+          mb="0.75rem"
+        />
+        <Stack spacing="0.5rem" mx="0.5rem">
+          <Text>{title}</Text>
+          <Text fontSize="sm" color="gray.500">{author}</Text>
+          <Text fontSize="sm" color="gray.500">{description}</Text>
+          <Flex justify="space-between" align="center">
+            <Box
+              fontSize="sm"
+              color="blue.500"
+              p="0.06rem"
+              borderRadius="4px"
+              bgGradient="linear(to-r, #A999FF, #73D5FF)"
+              bgClip="border"
+            >
+              <Text bg="gray.50" px="0.75rem" py="0.5rem" borderRadius="3.5px">{genre}</Text>
+            </Box>
+            <Text fontSize="sm" color="blue.500">{publishedAt}</Text>
+          </Flex>
+        </Stack>
       </Box>
     </Box>
   );
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -13,11 +13,54 @@ export default function Home() {
           spacingY="1.5rem"
           justifyContent="space-between"
         >
-          <BookCards />
-          <BookCards />
-          <BookCards />
-          <BookCards />
-          <BookCards />
+          <BookCards 
+            image="https://images.unsplash.com/photo-1532012197267-da84d127e765?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80"
+            title="Título" 
+            author="Autor"
+            description="Descrição do livro"
+            genre="Gênero"
+            publishedAt="21 Jun. 2022"
+          />
+          <BookCards 
+            image="https://images.unsplash.com/photo-1532012197267-da84d127e765?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80"
+            title="Título" 
+            author="Autor"
+            description="Descrição do livro"
+            genre="Gênero"
+            publishedAt="21 Jun. 2022"
+          />
+          <BookCards 
+            image="https://images.unsplash.com/photo-1532012197267-da84d127e765?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80"
+            title="Título" 
+            author="Autor"
+            description="Descrição do livro"
+            genre="Gênero"
+            publishedAt="21 Jun. 2022"
+          />
+          <BookCards 
+            image="https://images.unsplash.com/photo-1532012197267-da84d127e765?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80"
+            title="Título" 
+            author="Autor"
+            description="Descrição do livro"
+            genre="Gênero"
+            publishedAt="21 Jun. 2022"
+          />
+          <BookCards 
+            image="https://images.unsplash.com/photo-1532012197267-da84d127e765?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80"
+            title="Título" 
+            author="Autor"
+            description="Descrição do livro"
+            genre="Gênero"
+            publishedAt="21 Jun. 2022"
+          />
+          <BookCards 
+            image="https://images.unsplash.com/photo-1532012197267-da84d127e765?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80"
+            title="Título" 
+            author="Autor"
+            description="Descrição do livro"
+            genre="Gênero"
+            publishedAt="21 Jun. 2022"
+          />
         </SimpleGrid>
       </Box>
       <Footer />
